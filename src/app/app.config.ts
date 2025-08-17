d9e9c9c49a55ce75bd1b3559a0571179cf3bc791
Parisa <mail.hampacredit.com>
2025-08-17 14:43:28 +0330

    update to angular 19

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,24 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import { provideRouter, withNavigationErrorHandler, RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideRouter(routes), provideClientHydration(),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withNavigationErrorHandler((err) => {
+        console.error('Navigation error:', err);
+        // you could redirect to a fallback page here
+      })
+    ),
+    provideClientHydration(),
     importProvidersFrom([
       BrowserAnimationsModule,
       RouterModule,
     ])
   ]
-}
+};
